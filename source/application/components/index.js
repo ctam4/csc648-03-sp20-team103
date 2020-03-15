826e9ab816b06af13243f7a44365b3e83f2e5ed0
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-03-14 20:14:32 -0700

    Application patch framework (#40)
    * Replace webpack with parcel
    
    * Added "use strict"
    
    * Added .gitignore specifc for application
    
    * Updated docker-compose to use for local development only
    
    * Updated README
    
    * Fix port in app.js
    
    * Fix wrong folder at build-watch
    
    * Tighten security
    
    * ignore idea
    
    * Revert "ignore idea"
    
    This reverts commit f364440e3936aca1057f27aa72bbbd3aa5559d4b.
    
    * Update gitignore
    
    Co-authored-by: Corey Russ <cruss.seq11@gmail.com>
    Co-authored-by: Matthew <15389260+MatthewI123@users.noreply.github.com>

--- source/application/components/index.js
+++
@@ -1,5 +0,0 @@
-const react = require('react');
-const reactDOM = require('react-dom');
-const appComponent = require('./app.js');
-
-reactDOM.hydrate(appComponent, document.getElementById('reactElement'));
