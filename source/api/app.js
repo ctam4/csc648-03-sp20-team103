09a268b792e0a1183eae8619f7ca9ff7dc5b07ec
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-03-14 20:13:48 -0700

    Api patch framework (#41)
    * Added "use strict"
    
    * Updated docker-compose to use for local development only
    
    * Added DB_DATABASE environment var
    
    * Simpified code for routing
    
    * Added workaround for mariadb fails to init
    
    * Updated README
    
    * Tighten security
    
    * Update gitignore
    
    Co-authored-by: Matthew <15389260+MatthewI123@users.noreply.github.com>

--- source/api/app.js
+++ source/api/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const express = require('express');
 const http = require('http');
 const https = require('https');
@@ -10,11 +12,11 @@ const httpsOptions = {
   cert: fs.readFileSync('server.cert'),
 };
 const compression = require('compression');
-const indexRoute = require('./routes/index.js');
 
 const mariadb = require('mariadb');
 const mariadbOptions = {
   host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   connectionLimit: 5,
@@ -39,7 +41,7 @@ async function asyncFunction() {
 
 app.use(compression());
 
-app.use('/', indexRoute);
+app.use('/', require('./routes/index.js'));
 
 http.createServer(app).listen(httpPort);
 https.createServer(httpsOptions, app).listen(httpsPort);
