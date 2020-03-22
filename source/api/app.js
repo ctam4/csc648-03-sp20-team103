3831c351ec2a2b524f0e53060f6b3540f6863d2d
Kevin Cua <kevincua@Kevins-MacBook-Pro.local>
2020-03-22 15:05:33 -0700

    added migration files, routes

--- source/api/app.js
+++ source/api/app.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const express = require('express');
 const http = require('http');
 const https = require('https');
@@ -12,17 +10,20 @@ const httpsOptions = {
   cert: fs.readFileSync('server.cert'),
 };
 const compression = require('compression');
+const indexRoute = require('./routes/index.js');
 
 const mariadb = require('mariadb');
 const mariadbOptions = {
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  connectionLimit: 5,
+  host: "team103",
+  user: "team103",
+  password: "team103"
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // connectionLimit: 5,
 };
 const pool = mariadb.createPool(mariadbOptions);
-async function asyncFunction() {
+(async function asyncFunction() {
   let connection;
   try {
     connection = await pool.getConnection();
@@ -37,11 +38,35 @@ async function asyncFunction() {
       connection.release(); // release to pool
     }
   }
-}
+})();
+
+//users
+app.get('/users/:name', function (req, res) {
+  connection.query('select user_id from users where name =?', [req.params.name], function (error, results, fields) {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
+});
+
+app.post('/users/:name/:fridge_id/:role'{
+  var postData = req.body;
+  connection.query('INSERT INTO users SET ?', postData, function (error, results, fields) {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
+});
+//fridges
+app.get('/fridges/:user_id', function (req, res) {
+  connection.query('select fridge_id from fridges where user_id=?', [req.params.user_id], function (error, results, fields) {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
+});
 
 app.use(compression());
 
-app.use('/', require('./routes/index.js'));
+app.use('/', indexRoute);
 
 http.createServer(app).listen(httpPort);
 https.createServer(httpsOptions, app).listen(httpsPort);
+
