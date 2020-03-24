c505fb9c37153e9e34ad5a52856ec7648ec88b52
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-03-23 20:52:42 -0700

    Fix 4 spaces indentation

--- source/api/routes/v1/fridges.js
+++ source/api/routes/v1/fridges.js
@@ -7,39 +7,39 @@ let connection;
 //fridges
 
 fridges.get('/:user_id', function (req, res) {
-    try {
-        connection = await pool.getConnection();
-        await connection.query('select fridge_id from fridges where user_id =?', [req.query.user_id], function (error, results, fields) {
-            res.json(results.fridge_id);
-        });
-    } catch (error) {
-        res.sendStatus(500).end();
-        throw error;
-    } finally {
-        if (connection) {
-            connection.release(); // release to pool
-        }
+  try {
+    connection = await pool.getConnection();
+    await connection.query('select fridge_id from fridges where user_id =?', [req.query.user_id], function (error, results, fields) {
+      res.json(results.fridge_id);
+    });
+  } catch (error) {
+    res.sendStatus(500).end();
+    throw error;
+  } finally {
+    if (connection) {
+      connection.release(); // release to pool
     }
+  }
 });
 
 fridges.post('/', function (req, res) {
-    try {
-        connection = await pool.getConnection();
-        await connection.query('INSERT INTO fridges SET ?', req.body, function (error, results, fields) {
-            if (error) throw error;
-            res.json(results);
-        });
-    } catch (error) {
-        res.sendStatus(500).end();
-        throw error;
-    } finally {
-        if (connection) {
-            connection.release(); // release to pool
-        }
+  try {
+    connection = await pool.getConnection();
+    await connection.query('INSERT INTO fridges SET ?', req.body, function (error, results, fields) {
+      if (error) throw error;
+      res.json(results);
+    });
+  } catch (error) {
+    res.sendStatus(500).end();
+    throw error;
+  } finally {
+    if (connection) {
+      connection.release(); // release to pool
     }
+  }
 });
 
-//console.log('inventory.stack');
-//console.log(inventory.stack);
+//console.log('fridges.stack');
+//console.log(fridges.stack);
 
 module.exports = fridges;
--- source/api/routes/v1/users.js
+++ source/api/routes/v1/users.js
@@ -7,39 +7,39 @@ let connection;
 //users
 
 users.get('/:name', function (req, res) {
-    try {
-        connection = await pool.getConnection();
-        await connection.query('select user_id from users where name =?', [req.query.name], function (error, results, fields) {
-            res.json(results.user_id);
-        });
-    } catch (error) {
-        res.sendStatus(500).end();
-        throw error;
-    } finally {
-        if (connection) {
-            connection.release(); // release to pool
-        }
+  try {
+    connection = await pool.getConnection();
+    await connection.query('select user_id from users where name =?', [req.query.name], function (error, results, fields) {
+      res.json(results.user_id);
+    });
+  } catch (error) {
+    res.sendStatus(500).end();
+    throw error;
+  } finally {
+    if (connection) {
+      connection.release(); // release to pool
     }
+  }
 });
 
 users.post('/', function (req, res) {
-    try {
-        connection = await pool.getConnection();
-        await connection.query('INSERT INTO users SET ?', req.body, function (error, results, fields) {
-            if (error) throw error;
-            res.json(results);
-        });
-    } catch (error) {
-        res.sendStatus(500).end();
-        throw error;
-    } finally {
-        if (connection) {
-            connection.release(); // release to pool
-        }
+  try {
+    connection = await pool.getConnection();
+    await connection.query('INSERT INTO users SET ?', req.body, function (error, results, fields) {
+      if (error) throw error;
+      res.json(results);
+    });
+  } catch (error) {
+    res.sendStatus(500).end();
+    throw error;
+  } finally {
+    if (connection) {
+      connection.release(); // release to pool
     }
+  }
 });
 
-//console.log('inventory.stack');
-//console.log(inventory.stack);
+//console.log('users.stack');
+//console.log(users.stack);
 
 module.exports = users;
