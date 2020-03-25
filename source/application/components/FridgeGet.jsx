b0ee81bd73f7d9f8e8e75bb48003b860264ef1cb
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-03-24 19:27:42 -0700

    Add front end UI

---
+++ source/application/components/FridgeGet.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import createReactClass from 'create-react-class';
+
+export default React.createElement(createReactClass({
+  getInitialState: function() {
+    return {
+      msg: 'ready',
+    };
+  },
+  handleButtonClick: function(e) {
+    let url = location.protocol + '//' + location.hostname + ':8080/v1/fridges';
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('error ' + res.status);
+        }
+        return res.text();
+      })
+      .then((data) => {
+        this.setState({
+          msg: data
+        });
+      })
+      .catch((err) => this.setState({
+        msg: err
+      }));
+  },
+  render: function() {
+    let msg;
+    if (this.state.msg !== '') {
+      msg = <p>{this.state.msg}</p>;
+    } else {
+      msg = '';
+    }
+    return (
+      <div>
+        <p><label>FridgeGet</label></p>
+        <button id="btnSubmit" onClick={this.handleButtonClick}>Request</button>
+        <code>{msg}</code>
+      </div>
+    );
+  },
+}));
