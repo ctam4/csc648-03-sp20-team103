bf71cee24dfe315451e4f7691051f7d77e09a8be
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-03-24 22:40:21 -0700

    Fix apiUrl

--- source/application/components/FridgePost.jsx
+++ source/application/components/FridgePost.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import createReactClass from 'create-react-class';
 
+let apiUrl = process.env.API_HOST || location.hostname;
+if (process.env.API_PORT) {
+  apiUrl += ':' + process.env.API_PORT;
+}
+
 export default React.createElement(createReactClass({
   getInitialState: function() {
     return {
