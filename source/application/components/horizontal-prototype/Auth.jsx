979c082ccc28ca5845d09eb0f2b443b44760256e
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-04 15:20:14 -0700

    Fix import * as React

--- source/application/components/horizontal-prototype/Auth.jsx
+++ source/application/components/horizontal-prototype/Auth.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import CreateReactClass from 'create-react-class';
 import { connect } from 'react-redux';
 import LocalizedStrings from 'react-localization';
--- source/application/components/vertical-prototype/FridgeGet.jsx
+++ source/application/components/vertical-prototype/FridgeGet.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import CreateReactClass from 'create-react-class';
 
 let apiUrl = process.env.API_HOST || location.hostname;
