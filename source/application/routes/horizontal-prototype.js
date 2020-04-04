3fd6a1f80b1e8237b8af89e8e382a322b9e1e259
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-04 15:18:51 -0700

    Use handlebars for page title

--- source/application/routes/horizontal-prototype.js
+++ source/application/routes/horizontal-prototype.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   const raw = fs.readFileSync('./build/horizontal-prototype.html').toString();
   const handlebarsTemplate = handlebars.compile(raw);
   const render = handlebarsTemplate({
-    //root: reactDOMServer.renderToString(appComponent),
+    title: "Horizontal prototype",
   });
   res.send(render);
 });
