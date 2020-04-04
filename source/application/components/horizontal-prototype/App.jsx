d0442a5cffea99bafb27b84fb3e7a50b2456c86f
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-04 15:04:31 -0700

    Added redux react-redux redux-presist

--- source/application/components/horizontal-prototype/App.jsx
+++ source/application/components/horizontal-prototype/App.jsx
@@ -1,30 +1,19 @@
 import React from 'react';
 import CreateReactClass from 'create-react-class';
-import LocalizedStrings from 'react-localization';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-let apiUrl = process.env.API_HOST || location.hostname;
-if (process.env.API_PORT) {
-  apiUrl += ':' + process.env.API_PORT;
-}
-
-let strings = new LocalizedStrings({
-  en: {
-    hello_world: "Hello, world!",
-  },
-});
-
-const styles = StyleSheet.create({
-  box: { padding: 10 },
-  text: { fontWeight: 'bold' },
-});
+import { store, persistor } from '../../stores/horizontal-prototype/store';
+import Auth from './Auth.jsx';
 
 export default CreateReactClass({
   render: function() {
     return (
-      <View style={styles.box}>
-        <Text style={styles.text}>{strings.hello_world}</Text>
-      </View>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Auth />
+        </PersistGate>
+      </Provider>
     );
   },
 });
