2c75b24db4e1ca2501ec7c73a933fac4a7fb81a5
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-04 13:39:32 -0700

    Added react-localization support

--- source/application/components/horizontal-prototype/App.jsx
+++ source/application/components/horizontal-prototype/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import createReactClass from 'create-react-class';
+import LocalizedStrings from 'react-localization';
 import { StyleSheet, Text, View } from 'react-native';
 
 let apiUrl = process.env.API_HOST || location.hostname;
 if (process.env.API_PORT) {
   apiUrl += ':' + process.env.API_PORT;
 }
+
+let strings = new LocalizedStrings({
+  en: {
+    hello_world: "Hello, world!",
+  },
+});
 
 const styles = StyleSheet.create({
   box: { padding: 10 },
@@ -16,7 +23,7 @@ export default createReactClass({
   render: function() {
     return (
       <View style={styles.box}>
-        <Text style={styles.text}>Hello, world!</Text>
+        <Text style={styles.text}>{strings.hello_world}</Text>
       </View>
     );
   },
