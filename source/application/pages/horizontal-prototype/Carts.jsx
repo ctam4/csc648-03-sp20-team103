6265d8ab4f3925018df61e4f877dab2c3c41daed
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-18 00:35:36 -0700

    Code formatting with some styling fixes on new files

--- source/application/pages/horizontal-prototype/Carts.jsx
+++ source/application/pages/horizontal-prototype/Carts.jsx
@@ -2,44 +2,47 @@ import * as React from "react";
 import CreateReactClass from "create-react-class";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+
 import { store, persistor } from "../../stores/horizontal-prototype/store";
+
 import { StyleSheet, View, ScrollView } from "react-native";
 import LocalizedStrings from "react-localization";
+
 import AppHeader from "../../components/horizontal-prototype/AppHeader";
 import CartsCard from "../../components/horizontal-prototype/CartsCard";
 
 let strings = new LocalizedStrings({
   en: {
     carts: "Carts",
-    filter: "filter",
-    updated: "last updated 5 hours ago",
-    userCart: "User 1's cart",
-    previewCart: "this is the preview of the cart, maybe upto 10 lines of items with quantity",
-    edit: "EDIT",
-    clearCart: "CLEAR CART",
+    last_updated: "last updated",
+    user_cart: "'s cart",
+    preview_cart: "this is the preview of the cart, maybe upto 10 lines of items with quantity",
+    edit: "Edit",
+    clear_cart: "Clear cart",
   },
 });
-
 const styles = StyleSheet.create({
   container: {
     flex: 1
   },
   materialHeader1: {
-    width: 360,
+    minWidth: 360,
+    width: "100%",
     height: 56
   },
   scrollArea1: {
-    width: 360,
+    minWidth: 360,
+    width: "100%",
     height: 628,
     backgroundColor: "rgba(230, 230, 230,1)"
   },
   scrollArea1_contentContainerStyle: {
-    width: 360,
-    height: 3140,
+    minWidth: 360,
+    width: "100%",
     flexDirection: "column"
   },
   materialCardWithoutImage: {
-    width: 330,
+    minWidth: 330,
     height: 200,
     margin: 15
   }
@@ -48,35 +51,31 @@ const styles = StyleSheet.create({
 export default CreateReactClass({
   render: function() {
     return (
-     <Provider store={store}>
+      <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-            <View style={styles.container}>
-                <AppHeader
-                    icon2Name={strings.filter}
-                    text1={strings.carts}
-                    style={styles.materialHeader1}
-                ></AppHeader>
-                <View style={styles.scrollArea1}>
-                    <ScrollView
-                        contentContainerStyle={styles.scrollArea1_contentContainerStyle}
-                    >
-                        <CartsCard
-                            text2={strings.updated}
-                            text1={strings.userCart}
-                            text3={strings.previewCart}
-                            text4={strings.edit}
-                            text5={strings.clearCart}
-                            style={styles.materialCardWithoutImage}
-                        ></CartsCard>
-                    </ScrollView>
-                </View>
+          <View style={styles.container}>
+            <AppHeader
+              icon2Name={strings.filter}
+              text1={strings.carts}
+              style={styles.materialHeader1}
+            ></AppHeader>
+            <View style={styles.scrollArea1}>
+              <ScrollView
+                contentContainerStyle={styles.scrollArea1_contentContainerStyle}
+              >
+                <CartsCard
+                  text1={strings.user_cart}
+                  text2={strings.last_updated}
+                  text3={strings.preview_cart}
+                  text4={strings.edit}
+                  text5={strings.clear_cart}
+                  style={styles.materialCardWithoutImage}
+                ></CartsCard>
+              </ScrollView>
             </View>
-         </PersistGate>
+          </View>
+        </PersistGate>
       </Provider>
     );
   },
 });
-
-
-
-
--- source/application/pages/horizontal-prototype/Consumption.jsx
+++ source/application/pages/horizontal-prototype/Consumption.jsx
@@ -2,47 +2,47 @@ import * as React from "react";
 import CreateReactClass from "create-react-class";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+
 import { store, persistor } from "../../stores/horizontal-prototype/store";
+
 import { StyleSheet, View, ScrollView } from "react-native";
 import LocalizedStrings from "react-localization";
+
 import AppHeader from "../../components/horizontal-prototype/AppHeader";
 import ConsumptionCard from "../../components/horizontal-prototype/ConsumptionCard";
 
 let strings = new LocalizedStrings({
   en: {
     consumption: "Consumption",
-    calendarWeek: "calendar-week",
-    user: "User 1",
-    role: "{role}",
-    consumptionChart: "Above is a chart for the last 30 days. This text describes how user did compared to average person only on calories.",
-    viewDetails: "VIEW DETAILS",
+    view_details: "View details",
   },
 });
-
 const styles = StyleSheet.create({
   container: {
     flex: 1
   },
   materialHeader1: {
-    width: 360,
+    minWidth: 360,
+    width: "100%",
     height: 56,
     alignSelf: "center"
   },
   scrollArea1: {
-    width: 360,
+    minWidth: 360,
+    width: "100%",
     height: 628,
     backgroundColor: "rgba(230, 230, 230,1)"
   },
   scrollArea1_contentContainerStyle: {
-    width: 360,
-    height: 3140,
-    flexDirection: "column"
+    minWidth: 360,
+    width: "100%",
+    flexDirection: "column",
   },
   materialCard3: {
-    width: 330,
+    minWidth: 330,
     height: 420,
-    margin: 15
-  }
+    margin: 15,
+  },
 });
 
 export default CreateReactClass({
@@ -50,32 +50,27 @@ export default CreateReactClass({
     return (
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-           <View style={styles.container}>
-              <AppHeader
-                text1={strings.consumption}
-                icon2Name={strings.calendarWeek}
-                style={styles.materialHeader1}
-              ></AppHeader>
-              <View style={styles.scrollArea1}>
-                <ScrollView
-                    contentContainerStyle={styles.scrollArea1_contentContainerStyle}
-                >
-                    <ConsumptionCard
-                        text1={strings.user}
-                        text2={strings.role}
-                        text3={strings.consumptionChart}
-                        text4={strings.viewDetails}
-                        style={styles.materialCard3}
-                    ></ConsumptionCard>
-                </ScrollView>
-              </View>
-           </View>
-           </PersistGate>
-        </Provider>
+        <View style={styles.container}>
+          <AppHeader
+            text1={strings.consumption}
+            style={styles.materialHeader1}
+          ></AppHeader>
+          <View style={styles.scrollArea1}>
+            <ScrollView
+              contentContainerStyle={styles.scrollArea1_contentContainerStyle}
+            >
+              <ConsumptionCard
+                text1="user"
+                text2="role"
+                text3="Above is a chart for the last 30 days. This text describes how user did compared to average person only on calories."
+                text4={strings.view_details}
+                style={styles.materialCard3}
+              ></ConsumptionCard>
+            </ScrollView>
+          </View>
+        </View>
+        </PersistGate>
+      </Provider>
     );
   },
 });
-
-
-
-
