d895fa2e447410ab44f4a6982f5b2be13df44e22
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-29 02:16:32 -0700

    Fix RecipesCard

--- source/application/components/horizontal-prototype/RecipesCard.js
+++ source/application/components/horizontal-prototype/RecipesCard.js
@@ -1,94 +1,40 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Image, TouchableOpacity } from 'react-native';
-import { IconContext } from 'react-icons';
-import { MdEdit, MdFavorite, MdHistory } from 'react-icons/md';
+import { View } from 'react-native';
+
+import { Headline6, Subtitle2 } from './MaterialTypography';
+import MaterialIcon from '@material/react-material-icon';
+import MaterialCard, { CardPrimaryContent, CardMedia, CardActions, CardActionIcons } from './MaterialCard';
+import '@material/react-material-icon/dist/material-icon.css';
 
 function RecipesCard(props) {
   return (
-    <View style={[styles.container, props.style]}>
-      <Image
-        source={props.cardItemimage}
-        style={styles.cardItemImage}
-      ></Image>
-      <View style={styles.buttonGroup}>
-        <TouchableOpacity style={styles.leftBtn} onPress={props.onPressLeft}>
-          <IconContext.Provider value={{ style: iconStyles.icon1 }}>
-            <MdFavorite />
-          </IconContext.Provider>
-        </TouchableOpacity>
-        <TouchableOpacity /* Conditional navigation not supported at the moment */
-          style={styles.centerBtn}
-          onPress={props.onPressCenter}
-        >
-          <IconContext.Provider value={{ style: iconStyles.icon2 }}>
-            <MdEdit />
-          </IconContext.Provider>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.rightBtn} onPress={props.onPressRight}>
-          <IconContext.Provider value={{ style: iconStyles.icon3 }}>
-            <MdHistory />
-          </IconContext.Provider>
-        </TouchableOpacity>
-      </View>
-    </View>
+    <MaterialCard className='mdc-card'>
+      <CardPrimaryContent onClick={props.onClickMain}>
+        <CardMedia wide imageUrl={props.mainImage} style={{display: 'flex', alignItems: 'flex-end'}}>
+          <View style={{padding: 16, position: 'absolute', bottom: 0}}>
+            <Headline6 style={{margin: 0}}>{props.mainText1}</Headline6>
+            <Subtitle2 style={{margin: 0}}>{props.mainText2}</Subtitle2>
+          </View>
+        </CardMedia>
+      </CardPrimaryContent>
+      <CardActions>
+        <CardActionIcons>
+          <MaterialIcon
+            aria-label='favorite'
+            hasRipple
+            icon='favorite'
+            onClickAction1={props.onClickAction1}
+          />
+          <MaterialIcon
+            aria-label='history'
+            hasRipple
+            icon='history'
+            onClickAction1={props.onClickAction2}
+          />
+        </CardActionIcons>
+      </CardActions>
+    </MaterialCard>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: 185,
-    flex: 1,
-    backgroundColor: '#FFF',
-    flexWrap: 'nowrap',
-    elevation: 3,
-    borderRadius: 2,
-    borderColor: '#CCC',
-    borderWidth: 1,
-    shadowOffset: {
-      height: 2,
-      width: -2
-    },
-    shadowColor: '#000',
-    shadowOpacity: 0.1,
-    shadowRadius: 1.5,
-    overflow: 'hidden'
-  },
-  cardItemImage: {
-    flex: 1,
-    backgroundColor: '#ccc',
-    minHeight: 180
-  },
-  buttonGroup: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 8
-  },
-  leftBtn: {
-    padding: 8
-  },
-  centerBtn: {
-    padding: 8
-  },
-  rightBtn: {
-    padding: 8
-  }
-});
-const iconStyles = {
-  icon1: {
-    color: '#000',
-    fontSize: 24,
-    opacity: 0.5
-  },
-  icon2: {
-    color: '#000',
-    fontSize: 24,
-    opacity: 0.5
-  },
-  icon3: {
-    color: '#000',
-    fontSize: 24,
-    opacity: 0.5
-  }
-};
 
 export default RecipesCard;
