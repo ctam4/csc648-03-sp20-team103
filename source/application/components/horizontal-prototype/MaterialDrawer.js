362e56acc78c151b3814d890e45f2fbbbe3b0219
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-29 22:26:04 -0700

    Added Users, Changed icons at Drawer

--- source/application/components/horizontal-prototype/MaterialDrawer.js
+++ source/application/components/horizontal-prototype/MaterialDrawer.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from 'react';
 import Drawer, { DrawerContent, DrawerHeader, DrawerTitle } from '@material/react-drawer';
-import List, { ListItem, ListItemGraphic, ListItemText } from '@material/react-list';
+import List, { ListDivider, ListItem, ListItemGraphic, ListItemText } from '@material/react-list';
 import MaterialIcon from '@material/react-material-icon';
 import '@material/react-drawer/dist/drawer.css';
 import '@material/react-list/dist/list.css';
@@ -9,12 +9,12 @@ import LocalizedStrings from 'react-localization';
 
 let strings = new LocalizedStrings({
   en: {
-    meal_plans: 'Meal Plans',
     inventory: 'Inventory',
-    consumption: 'Consumption',
-    carts: 'Carts',
-    users: 'Users',
     recipes: 'Recipes',
+    carts: 'Carts',
+    consumption: 'Consumption',
+    meal_plans: 'Meal Plans',
+    users: 'Users',
   },
 });
 
@@ -30,25 +30,30 @@ function MaterialDrawer(props) {
       </DrawerHeader>
       <DrawerContent>
         <List singleSelection selectedIndex={selectedIndex}>
-          <ListItem  onClick={() => window.location.href = './meal-plans'}>
-            <ListItemGraphic graphic={<MaterialIcon icon='folder'/>} />
-            <ListItemText primaryText={strings.meal_plans} />
-          </ListItem>
-          <ListItem  onClick={() => window.location.href = './inventory'}>
-            <ListItemGraphic graphic={<MaterialIcon icon='list_alt'/>} />
+          <ListItem onClick={() => window.location.href = './inventory'}>
+            <ListItemGraphic graphic={<MaterialIcon icon='kitchen'/>} />
             <ListItemText primaryText={strings.inventory} />
           </ListItem>
-          <ListItem onClick={() => window.location.href = './consumption'}>
-            <ListItemGraphic graphic={<MaterialIcon icon='bar_chart'/>} />
-            <ListItemText primaryText={strings.consumption} />
+          <ListItem onClick={() => window.location.href = './recipes'} >
+            <ListItemGraphic graphic={<MaterialIcon icon='list_alt'/>} />
+            <ListItemText primaryText={strings.recipes} />
           </ListItem>
           <ListItem onClick={() => window.location.href = './carts'}>
             <ListItemGraphic graphic={<MaterialIcon icon='shopping_cart'/>} />
             <ListItemText primaryText={strings.carts} />
           </ListItem>
-          <ListItem onClick={() => window.location.href = './recipes'} >
-            <ListItemGraphic graphic={<MaterialIcon icon='description'/>} />
-            <ListItemText primaryText={strings.recipes} />
+          <ListItem onClick={() => window.location.href = './consumption'}>
+            <ListItemGraphic graphic={<MaterialIcon icon='bar_chart'/>} />
+            <ListItemText primaryText={strings.consumption} />
+          </ListItem>
+          <ListItem onClick={() => window.location.href = './meal-plans'}>
+            <ListItemGraphic graphic={<MaterialIcon icon='menu_book'/>} />
+            <ListItemText primaryText={strings.meal_plans} />
+          </ListItem>
+          <ListDivider tag="div" />
+          <ListItem onClick={() => window.location.href = './users'}>
+            <ListItemGraphic graphic={<MaterialIcon icon='people'/>} />
+            <ListItemText primaryText={strings.users} />
           </ListItem>
         </List>
       </DrawerContent>
