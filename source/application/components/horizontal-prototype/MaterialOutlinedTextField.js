d31e8609f0c45e67e75360b4a1e6e6389e8a94c7
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-02 16:48:18 -0700

    Split Textfield to two variants

---
+++ source/application/components/horizontal-prototype/MaterialOutlinedTextField.js
@@ -0,0 +1,21 @@
+import React, { Component, useState } from 'react';
+import TextField, { HelperText, Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
+import '@material/react-text-field/dist/text-field.css';
+import '@material/react-material-icon/dist/material-icon.css';
+
+function MaterialOutlinedTextField(props) {
+  return (
+    <TextField
+      outlined
+      label={props.label}
+      helperText={<HelperText>{props.helperText}</HelperText>}
+      onTrailingIconSelect={props.onTrailingIconSelect}
+      trailingIcon={<MaterialIcon role='button' icon={props.trailingIcon || 'clear'} />}
+    >
+      <Input inputType={props.inputType} value={props.value} onChange={props.onChange} />
+    </TextField>
+  );
+}
+
+export default MaterialOutlinedTextField;
