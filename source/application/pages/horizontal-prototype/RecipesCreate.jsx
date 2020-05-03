a3e5964034ed09abcc99b46c303f98ff67a14b2b
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-03 02:28:27 -0700

    Replace RecipesCreate FixedLabelTextbox for MaterialOutlinedTextField

--- source/application/pages/horizontal-prototype/RecipesCreate.jsx
+++ source/application/pages/horizontal-prototype/RecipesCreate.jsx
@@ -16,15 +16,16 @@ import '@material/react-layout-grid/dist/layout-grid.css';
 
 import MaterialTopAppBarDialog from '../../components/horizontal-prototype/MaterialTopAppBarDialog';
 import MaterialFab from '../../components/horizontal-prototype/MaterialFab';
-import FixedLabelTextbox from '../../components/horizontal-prototype/FixedLabelTextbox';
+import MaterialOutlinedTextField from '../../components/horizontal-prototype/MaterialOutlinedTextField';
 
 let strings = new LocalizedStrings({
   en: {
-    recipe_info: 'Recipe info',
     name: 'Name',
+    name_helper: 'This is the name of the recipe.',
     servings: 'Servings',
+    servings_helper: 'This is the servings of this recipe.',
     cooking_time: 'Cooking time',
-    ingredients: 'Ingredients',
+    cooking_time_helper: 'This is the cooking time of this recipe in minutes.',
   },
 });
 const styles = StyleSheet.create({
@@ -93,42 +94,38 @@ export default () => {
         <DrawerAppContent className='drawer-app-content'>
           <Grid style={{ height: useWindowDimensions().height - 64 }}>
             <Row>
-              <Cell desktopColumns={6} phoneColumns={4} tabletColumns={4}>
-              <Text style={styles.recipeInfo}>{strings.recipe_info}</Text>
+              <Cell columns={12}>
+                <MaterialOutlinedTextField
+                  label={strings.name}
+                  helperText={strings.name_helper}
+                  value={state.name}
+                  onChange={(e) => dispatch(setName(e.target.value))}
+                  onTrailingIconSelect={() => dispatch(setName(''))}
+                ></MaterialOutlinedTextField>
               </Cell>
               <Cell desktopColumns={6} phoneColumns={4} tabletColumns={4}>
-              <FixedLabelTextbox
-                text1={strings.name}
-                textInput1={state.name}
-                style={styles.materialFixedLabelTextbox}
-                onChange={(e) => dispatch(setName(e.target.value))}
-              ></FixedLabelTextbox>
+                <MaterialOutlinedTextField
+                  label={strings.servings}
+                  helperText={strings.servings_helper}
+                  value={state.servings}
+                  onChange={(e) => dispatch(setServings(e.target.value))}
+                  onTrailingIconSelect={() => dispatch(setServings(''))}
+                ></MaterialOutlinedTextField>
               </Cell>
               <Cell desktopColumns={6} phoneColumns={4} tabletColumns={4}>
-              <FixedLabelTextbox
-                text1={strings.servings}
-                textInput1={state.servings}
-                style={styles.materialFixedLabelTextbox1}
-                onChange={(e) => dispatch(setServings(e.target.value))}
-              ></FixedLabelTextbox>
-              </Cell>
-              <Cell desktopColumns={6} phoneColumns={4} tabletColumns={4}>
-              <FixedLabelTextbox
-                text1={strings.cooking_time}
-                textInput1={state.cooking_time}
-                style={styles.materialFixedLabelTextbox2}
-                onChange={(e) => dispatch(setCookingTime(e.target.value))}
-              ></FixedLabelTextbox>
-              </Cell>
-              <Cell desktopColumns={6} phoneColumns={4} tabletColumns={4}>
-              <Text style={styles.ingredients3}>{strings.ingredients}</Text>
+                <MaterialOutlinedTextField
+                  label={strings.cooking_time}
+                  helperText={strings.cooking_time_helper}
+                  value={state.cooking_time}
+                  onChange={(e) => dispatch(setCookingTime(e.target.value))}
+                  onTrailingIconSelect={() => dispatch(setCookingTime(''))}
+                ></MaterialOutlinedTextField>
               </Cell>
             </Row>
           </Grid>
         </DrawerAppContent>
-
         <MaterialFab
-          icon={<MaterialIcon icon='check'/>}
+          icon={<MaterialIcon icon='check' />}
           style={{ position: 'absolute', right: 16, bottom: 16 }}
           onClick={handleSave}
         ></MaterialFab>
