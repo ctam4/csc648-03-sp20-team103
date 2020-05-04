74bcac6464e5b41faf380e1e845466c7d1a228e4
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-04 02:08:29 -0700

    Fix and improve login & fridges

--- source/api/routes/v3/fridges.js
+++ source/api/routes/v3/fridges.js
@@ -7,20 +7,21 @@ let connection;
 /**
  * POST /v3/fridges
  * @description Creates a new fridge and yields its serial number and pin.
- * @returns {string} The serial number.
+ * @returns {string} serialNumber
+ * @returns {integer} pin
  */
 fridges.post('/', async (req, res) => {
+  const generate = (n) => Array(n).fill().map(() => Math.floor(10 * Math.random())).join(''); // @todo is there a better way?
+  let serialNumber, results;
+  const pin = generate(4);
+  // run query to mariadb
   try {
-    const generate = (n) => Array(n).fill().map(() => Math.floor(10 * Math.random())).join(''); // @todo is there a better way?
-    let serialNumber;
-    let results;
-    const pin = generate(4);
     connection = await pool.getConnection();
     do {
       results = await connection.query('SELECT 1 FROM v3_fridges WHERE serial_number=?', [serialNumber = generate(10)]);
     } while (results.length > 0);
     await connection.query('INSERT INTO v3_fridges (serial_number, pin) VALUES (?, ?)', [serialNumber, pin]);
-    res.send(JSON.stringify({ serialNumber: serialNumber, pin: pin })).end();
+    res.json({ serialNumber: serialNumber, pin: pin }).end();
   } catch (error) {
     res.sendStatus(500).end();
     throw error;
--- source/api/routes/v3/login.js
+++ source/api/routes/v3/login.js
@@ -10,33 +10,47 @@ let connection;
  * @description
  * @param {string} serialNumber
  * @param {string} pin
+ * @return {string} session
  */
 login.post('/', async (req, res) => {
-  if (!('serialNumber' in req.body && 'pin' in req.body)) {
+  // check correct params
+  if (Object.keys(req.body).length == 2 && !('serialNumber' in req.body && 'pin' in req.body)) {
     res.sendStatus(400).end();
     return;
   }
+  // check params data type
+  let serialNumber, pin;
+  try {
+    serialNumber = req.body.serialNumber;
+    pin = req.body.pin;
+  } catch (error) {
+    res.sendStatus(400).end();
+    throw error;
+  }
+  // check params data range
+  if (serialNumber.length > 16 || pin.length > 16) {
+    res.sendStatus(400).end();
+    return;
+  }
+  // run query to mariadb
   try {
     connection = await pool.getConnection();
-    connection.query('SELECT fridge_id FROM v3_fridges WHERE serial_number=? AND pin=?', [req.body.serialNumber, req.body.pin])
-      .then(async rows => {
+    connection.query('SELECT fridge_id FROM v3_fridges WHERE serial_number=? AND pin=?', [serialNumber, pin])
+      .then(async (rows) => {
         if (rows.length > 0) {
           // @todo handle possible duplicate sessions
           const fridgeID = rows[0].fridge_id;
-          const session = uuid.v4();
+          const session = uuidv4();
           const results = (await connection.query('SELECT CURRENT_TIMESTAMP as logged_in_ts, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 1 MONTH) as expires_ts'))[0];
           await connection.query('INSERT INTO v3_sessions(session, fridge_id, logged_in_ts, expires_ts) VALUES (?, ?, ?, ?)', [session, fridgeID, results.logged_in_ts, results.expires_ts]);
-          res.send({ ...results, session: session }).end();
+          res.json({ ...results, session: session }).end();
         } else {
-          res.sendStatus(404).end();
+          res.sendStatus(406).end();
         }
-      })
-      .catch(error => {
-      throw error;
-    });
+      });
   } catch (error) {
-    console.log(error);
     res.sendStatus(500).end();
+    throw error;
   } finally {
     if (connection) {
       connection.release();
