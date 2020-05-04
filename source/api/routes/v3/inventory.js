5329e3e42755979252eb06a6331130e029975b9b
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-04 03:44:44 -0700

    Fix /inventory/add/manual

--- source/api/routes/v3/inventory.js
+++ source/api/routes/v3/inventory.js
@@ -121,30 +121,69 @@ inventory.get('/', async (req, res) => {
   }
 });
 
-// ('INSERT INTO v3_inventory (fridge_id, ingredient_id, quantity, unit, expiration_date, price, state) VALUES(?,?,  ?, ?, ?, ?, ?)', [req.body.fridge_id, req.body.ingredient_id, req.body.quantity, req.body.unit, req.body.expiration_date, req.body.price, req.body.state])
-//stuck sending request
-inventory.post('/manual', async (req, res) => {
+/**
+ * POST /v3/inventory/add/manual
+ * @description Insert inventory for current fridges with session.
+ * @param {string} session
+ * @param {integer} ingredientID
+ * @param {timestamp} expirationDate
+ * @param {float} quantity
+ * @param {string} unit
+ * @param {integer} price
+ * @param {string} state
+ * @returns {interger} inventoryID
+ */
+inventory.post('/add/manual', async (req, res) => {
+  // check correct params
+  if (Object.keys(req.body).length == 7 && !('session' in req.body && 'ingredientID' in req.body && 'expirationDate' in req.body && 'quantity' in req.body && 'unit' in req.body && 'price' in req.body && 'state' in req.body)) {
+    res.sendStatus(400).end();
+    return;
+  }
+  // check params data type
+  let session, ingredientID, expirationDate, quantity, unit, price, state;
   try {
-    connection = await pool.getConnection();
-    body = Object.keys(req.body)
-    if (req.body.state !== undefined) {
-      let sql = 'INSERT INTO v3_inventory (fridge_id, ingredient_id, quantity, unit, expiration_date, price, state) VALUES(?,?,  ?, ?, ?, ?, ?)'
-      await connection.query(sql, [req.body.fridge_id, req.body.ingredient_id, req.body.quantity, req.body.unit, req.body.expiration_date, req.body.price, req.body.state])
-        .then((results) => {
-          res.send(JSON.stringify(results)).end();
-          // res.json(results).end();
-        });
-    }
-    else if (req.body.state === undefined) {
-      let sql = 'INSERT INTO v3_inventory (fridge_id, ingredient_id, quantity, unit, expiration_date, price) VALUES(?,?,  ?, ?, ?, ?)'
-      await connection.query(sql, [req.body.fridge_id, req.body.ingredient_id, req.body.quantity, req.body.unit, req.body.expiration_date, req.body.price])
-        .then((results) => {
-          res.send(JSON.stringify(results)).end();
-          // res.json(results).end();
-        });
-    }
+    session = req.body.session;
+    ingredientID = parseInt(req.body.ingredientID);
+    expirationDate = parseInt(req.body.expirationDate);
+    quantity = parseFloat(req.body.quantity);
+    unit = req.body.unit;
+    price = parseInt(req.body.price);
+    state = req.body.state;
   } catch (error) {
     res.sendStatus(400).end();
+    throw error;
+  }
+  // check params data range
+  // @todo unit valid values
+  if (ingredientID <= 0 || quantity <= 0.0 || price <= 0 || !['consumed', 'stored', 'discarded'].includes(state)) {
+    res.sendStatus(400).end();
+    return;
+  }
+  // run query to mariadb
+  try {
+    connection = await pool.getConnection();
+    // retrieve fridge_id
+    connection.query('SELECT fridge_id FROM v3_sessions WHERE session=?', [session])
+      .then(async (rows) => {
+        if (rows.length > 0) {
+          // @todo handle possible duplicate sessions
+          const fridgeID = rows[0].fridge_id;
+          // insert for endpoint
+          await connection.query('INSERT INTO v3_inventory (fridge_id, ingredient_id, expiration_date, quantity, unit, expiration_date, price, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [fridgeID, ingredientID, expirationDate, quantity, unit, price, state])
+            .then((rows) => {
+              if (rows.length > 0) {
+                // res.send(JSON.stringify(rows)).end();
+                res.json(rows).end();
+              } else {
+                res.sendStatus(406).end();
+              }
+            });
+        } else {
+          res.sendStatus(401).end();
+        }
+      });
+  } catch (error) {
+    res.sendStatus(500).end();
     throw error;
   } finally {
     if (connection) {
