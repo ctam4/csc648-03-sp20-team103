a3b39eca84f585d4611915b640142e72467c8ce5
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-04 04:12:17 -0700

    Fix GET /users

--- source/api/routes/v3/users.js
+++ source/api/routes/v3/users.js
@@ -4,16 +4,51 @@ const users = express.Router();
 const pool = require('../../database.js');
 let connection;
 
+/**
+ * GET /v3/users
+ * @description Retrieve users list of current fridges with session.
+ * @param {string} session
+ * @returns {object[]} users
+ */
 users.get('/', async (req, res) => {
+  // check correct params
+  if (Object.keys(req.body).length == 1 && !('session' in req.query)) {
+    res.sendStatus(400).end();
+    return;
+  }
+  // check params data type
+  let session;
+  try {
+    session = req.query.session;
+  } catch (error) {
+    res.sendStatus(400).end();
+    throw error;
+  }
+  // run query to mariadb
   try {
     connection = await pool.getConnection();
-    await connection.query('SELECT * FROM v3_users')
-      .then((results) => {
-        res.send(JSON.stringify(results)).end();
-        // res.json(results).end();
+    // retrieve fridge_id
+    connection.query('SELECT fridge_id FROM v3_sessions WHERE session=?', [session])
+      .then(async (rows) => {
+        if (rows.length > 0) {
+          // @todo handle possible duplicate sessions
+          const fridgeID = rows[0].fridge_id;
+          // retrieve for endpoint
+          await connection.query('SELECT * FROM v3_users WHERE fridge_id=?', [fridgeID])
+            .then((rows) => {
+              if (rows.length > 0) {
+                // res.send(JSON.stringify(rows)).end();
+                res.json(rows).end();
+              } else {
+                res.sendStatus(406).end();
+              }
+            });
+        } else {
+          res.sendStatus(401).end();
+        }
       });
   } catch (error) {
-    res.sendStatus(401).end();
+    res.sendStatus(500).end();
     throw error;
   } finally {
     if (connection) {
