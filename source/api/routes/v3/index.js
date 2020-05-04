ae2c0e05bca824b8141236791c9345b1c4f81983
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-04 04:22:26 -0700

    Reorder code

--- source/api/routes/v3/index.js
+++ source/api/routes/v3/index.js
@@ -4,11 +4,11 @@ const index = express.Router();
 index.get('/', (req, res) => res.sendStatus(401));
 
 index.use('/register', require('./register.js'));
+index.use('/login', require('./login.js'));
+index.use('/logout', require('./logout.js'));
 index.use('/users', require('./users.js'));
 index.use('/inventory', require('./inventory.js'));
-index.use('/login', require('./login.js'));
 index.use('/ingredients', require('./ingredients.js'));
-index.use('/logout', require('./logout.js'));
 
 //console.log('index.stack');
 //console.log(index.stack);
