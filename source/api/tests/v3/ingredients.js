322dd02a65d9dd0e43e468404e719d159bb02fa9
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-09 23:32:07 -0700

    Fix ingredients test

--- source/api/tests/v3/ingredients.js
+++ source/api/tests/v3/ingredients.js
@@ -57,6 +57,109 @@ test.before(async (t) => {
     });
 });
 
+test('/ingredients | GET | 400', async (t) => {
+  await fetch(t.context.baseUrl + '/v3/ingredients?session=abcd&ingredientIDs=12345678', {
+    method: 'get',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => {
+      t.is(res.status, 400);
+    });
+});
+
+test('/ingredients | GET | 401', async (t) => {
+  await fetch(t.context.baseUrl + '/v3/ingredients?session=123456789012345678901234567890123456&ingredientIDs=12345678', {
+    method: 'get',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => {
+      t.is(res.status, 401);
+    });
+});
+
+test('/ingredients | GET | 406', async (t) => {
+  await fetch(t.context.baseUrl + '/v3/register', {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then(async (data) => {
+      await fetch(t.context.baseUrl + '/v3/login', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          serialNumber: data.serialNumber,
+          pin: data.pin,
+        }),
+      })
+        .then((res2) => res2.json())
+        .then(async (data2) => {
+          await fetch(t.context.baseUrl + '/v3/ingredients?session=' + data2.session + '&ingredientIDs=12345678', {
+            method: 'get',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+          })
+            .then((res3) => {
+              t.is(res3.status, 406);
+            });
+        });
+    });
+});
+
+test('/ingredients | GET | 200', async (t) => {
+  await fetch(t.context.baseUrl + '/v3/ingredients', {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      ingredientID: Math.floor(Math.random() * 10000000),
+      name: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+      image: 'image.jpg',
+      session: t.context.session,
+    }),
+  })
+    .then((res) => res.json())
+    .then(async (data) => {
+      await fetch(t.context.baseUrl + '/v3/ingredients?session=' + t.context.session + '&ingredientIDs=' + data.ingredientID, {
+        method: 'get',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }
+      })
+        .then((res2) => {
+          t.is(res2.status, 200);
+          return res2.json();
+        })
+        .then((data2) => {
+          t.true(data2.length >= 1);
+          t.is(Object.keys(data2[0]).length, 3);
+          t.true('ingredientID' in data2[0]);
+          t.true('name' in data2[0]);
+          t.true('image' in data2[0]);
+          t.is(typeof data2[0].ingredientID, 'number');
+          t.is(typeof data2[0].name, 'string');
+          t.is(typeof data2[0].image, 'string');
+        });
+    });
+});
+
 test('/ingredients/search | GET | 400', async (t) => {
   await fetch(t.context.baseUrl + '/v3/ingredients/search?session=abcd&query=apple', {
     method: 'get',
@@ -216,152 +319,4 @@ test('/ingredients | POST | 200', async (t) => {
       t.true('ingredientID' in data);
       t.is(typeof data.ingredientID, 'number');
     });
-});
-
-test('/ingredients | GET | 400', async (t) => {
-  await fetch(t.context.baseUrl + '/v3/ingredients/search?session=123456789012345678901234567890123456&ingredientIDs=12345678', {
-    method: 'get',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((res) => {
-      t.is(res.status, 400);
-    });
-});
-
-test('/ingredients | GET | 401', async (t) => {
-  await fetch(t.context.baseUrl + '/v3/ingredients', {
-    method: 'post',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      ingredientID: Math.floor(Math.random() * 10000000),
-      name: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
-      image: 'image.jpg',
-      session: t.context.session,
-    }),
-  })
-    .then((res) => {
-      t.is(res.status, 200);
-      return res.json();
-    })
-    .then((data) => {
-      t.context.ingredientID1 = data.ingredientID;
-    }).then(async () => {
-      await fetch(t.context.baseUrl + '/v3/ingredients?session=123456789012345678901234567890123456'+'&ingredientIDs='+t.context.ingredientID1, {
-      method: 'get',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-    .then((res) => {
-      t.is(res.status, 401);
-    });
-  });
-});
-
-test('/ingredients | GET | 406', async (t) => {
-  await fetch(t.context.baseUrl + '/v3/ingredients?session='+t.context.session+'&ingredientIDs=12345678', {
-    method: 'get',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((res) => {
-      t.is(res.status, 406);
-    });
-});
-
-test('/ingredients | GET | 200', async (t) => {
-  await fetch(t.context.baseUrl + '/v3/ingredients', {
-    method: 'post',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      ingredientID: Math.floor(Math.random() * 10000000),
-      name: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
-      image: 'image.jpg',
-      session: t.context.session,
-    }),
-  })
-  .then((res) => {
-    t.is(res.status, 200);
-    return res.json();
-  })
-  .then((data) => {
-    t.context.ingredientID1 = data.ingredientID;
-  }).then(async () => {
-    await fetch(t.context.baseUrl + '/v3/ingredients?session='+t.context.session+'&ingredientIDs='+t.context.ingredientID1, {
-      method: 'get',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      }
-    })
-    .then((res) => {
-      t.is(res.status, 200);
-      return res.json();
-    })
-    .then((data) => {
-      t.true(data.length >= 1);
-      t.is(Object.keys(data[0]).length, 3);
-      t.true('ingredientID' in data[0]);
-      t.true('name' in data[0]);
-      t.true('image' in data[0]);
-      t.is(typeof data[0].ingredientID, 'number');
-      t.is(typeof data[0].name, 'string');
-      t.is(typeof data[0].image, 'string');
-    }).then(async () => {
-      await fetch(t.context.baseUrl + '/v3/ingredients', {
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          ingredientID: Math.floor(Math.random() * 10000000),
-          name: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
-          image: 'image.jpg',
-          session: t.context.session,
-        }),
-      })
-        .then((res) => {
-          t.is(res.status, 200);
-          return res.json();
-        })
-        .then((data) => {
-          t.context.ingredientID2 = data.ingredientID;
-        }).then(async () => {
-          await fetch(t.context.baseUrl + '/v3/ingredients?session='+t.context.session+'&ingredientIDs='+t.context.ingredientID1 + ',' +t.context.ingredientID2, {
-            method: 'get',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-            }
-          })
-          .then((res) => {
-            t.is(res.status, 200);
-            return res.json();
-          })
-          .then((data) => {
-            t.true(data.length >= 2);
-            t.is(Object.keys(data[0]).length, 3);
-            t.true('ingredientID' in data[0]);
-            t.true('name' in data[0]);
-            t.true('image' in data[0]);
-            t.is(typeof data[0].ingredientID, 'number');
-            t.is(typeof data[0].name, 'string');
-            t.is(typeof data[0].image, 'string');
-        })
-      });
-    });
-  });
 });
